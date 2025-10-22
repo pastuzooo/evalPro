cf3bb4e7f68078fb9af4e93b26efcfd1f602c7d7
pastuzooo <118612928+pastuzooo@users.noreply.github.com>
2025-10-22 13:53:01 +0200

    initial commit

---
+++ src/main/java/com/berufsschule/evalpro/EvalProApplication.java
@@ -0,0 +1,13 @@
+package com.berufsschule.evalpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvalProApplication.class, args);
+    }
+
+}
